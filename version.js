dd261cf570d1a262e3949b6af556383dcdf89b72
zswang <wjhu111@21cn.com>
2016-05-16 19:35:17 +0800

    Update version script.

---
+++ version.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+var path = require('path');
+
+var filename = path.join(__dirname, 'package.json');
+var package = JSON.parse(fs.readFileSync(filename));
+package.version = package.version.replace(/-?\d+$/, function(value) {
+    return parseInt(value) + 1;
+});
+package.main = package.name + '.js',
+fs.writeFileSync(filename, JSON.stringify(package, null, '  '));
+
+var bower_filename = path.join(__dirname, 'bower.json');
+var bower_package = JSON.parse(fs.readFileSync(bower_filename));
+bower_package.name = package.name;
+bower_package.description = package.description;
+bower_package.keywords = package.keywords;
+bower_package.version = package.version;
+fs.writeFileSync(bower_filename, JSON.stringify(bower_package, null, '  '));
